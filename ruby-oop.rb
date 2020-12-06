ee7e094eda02afb46b6d810251a8b84379a29416
Poncho <jplyles1212@gmail.com>
2020-12-05 17:01:32 -0800

    Add class for some public/private method practice

--- ruby-oop.rb
+++ ruby-oop.rb
@@ -84,3 +84,39 @@ jiminy = Cricket.new("Jiminy")
 
 peter.jump
 jiminy.jump
+
+#################################################################
+
+# Create your module here!
+module MartialArts
+  def swordsman
+    puts "I'm a swordsman."
+  end
+end
+
+class Ninja
+  include MartialArts
+  def initialize(clan)
+    @clan = clan
+  end
+end
+
+class Samurai
+  include MartialArts
+  def initialize(shogun)
+    @shogun = shogun
+  end
+end
+
+module ThePresent
+  def now
+    puts "It's #{Time.now.hour > 12 ? Time.new.hour - 12 : Time.new.hour}:#{Time.new.min}
+    #{Time.new.hour > 12 ? 'PM' : 'AM'} (GMT)."
+  end
+end
+
+class TheHereAnd
+  extend ThePresent
+end
+
+TheHereAnd.now
